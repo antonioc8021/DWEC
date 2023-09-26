5ad1632f1970781d7ba30bc1db54363208486a39
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-26 09:10:29 +0200

    finish ejer1

--- DWEC0201/Ejer1.js
+++ DWEC0201/Ejer1.js
@@ -5,3 +5,36 @@ function principal() {
         manejadorClick;
 }
 
+function manejadorClick() {
+    var salida;
+    let entrada = parseInt(document.getElementById("entrada").value);
+    if (esNumero(entrada)) {
+        if (esEntero(entrada)) {
+            salida = "Lo que has introducido es un número entero";
+        } if (esPositivo(entrada)) {
+            salida = "Lo que has introducido es un número positivo"
+        }
+    } else {
+        salida = "Lo que has introducido es una cadena de texto";
+
+    }
+
+    document.getElementById("salida").innerHTML = salida;
+
+}
+
+function esNumero(entrada) {
+    let res = false;
+    if (!isNaN(entrada)) {
+        res = true;
+    }
+    return res;
+}
+
+function esEntero(numero) {
+    return Number.isInteger(numero);
+}
+
+function esPositivo(numero) {
+    return numero > 0;
+}
