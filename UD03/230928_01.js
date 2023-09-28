a54703238b56a9ecb00aaf1eebf9242becc92d68
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 10:40:04 +0200

    function with setTimeout

--- UD03/230928_01.js
+++ UD03/230928_01.js
@@ -1,10 +1,18 @@
 window.onload = principal;
+
+let miliTimeOut;
+
 function principal() {
-    document.getElementById("miBoton").onclick =
-        manejadorClick;
+    document.getElementById("miBoton").onclick = manejadorClick;
+    miliTimeOut = setTimeout(inactive, 3000);
 }
 function manejadorClick() {
-    document.getElementById("salida").innerHTML =
-        "Has escrito: " +
-        document.getElementById("entrada").value;
+    clearTimeout(miliTimeOut);
+    miliTimeOut = setTimeout(inactive, 2000);
+    document.getElementById("salida").innerHTML = "Estamos activo papi";
+
+}
+
+function inactive() {
+    document.getElementById("salida").innerHTML = "Estás afk";
 }
