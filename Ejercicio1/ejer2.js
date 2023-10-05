afa96c80c36a6eefda20b86adbd72c2d2ad70ca8
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-05 11:30:37 +0200

    todo at home

---
+++ Ejercicio1/ejer2.js
@@ -0,0 +1,17 @@
+window.onload = principal
+
+function principal() {
+    document.getElementById("boton").onclick = manejadorClick;
+}
+
+function manejadorClick() {
+    document.getElementById("salida").innerHTML = "";
+}
+
+function aleatrorio(numero) {
+    let decimosComprados = [];
+
+    for (let i = 0; i < numero; i++) {
+        decimosComprados
+    }
+}
