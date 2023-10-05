5a5f03d00a0492279e0f6ba0b4a2d9c581e591b3
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-05 09:22:26 +0200

    funtion timepoVacaciones return correctly format

--- Ejercicio1/ejer1.js
+++ Ejercicio1/ejer1.js
@@ -1,12 +1,29 @@
 // Tarea DAWC03.01 - Ejercicio 3
+// ejemplo pillado de clase
 // Antonio Costas Salazar
 
 window.onload = principal;
 
 function principal() {
-    console.log("Hola mundo");
+    console.log(tiempoVacaciones(new Date("2024-01-01")));
 }
 
-function tiempoVacaciones() {
+function tiempoVacaciones(fechaFin) {
+    let fechaActual = new Date();
+    let msTotales = fechaFin - fechaActual;
+    // milisegundos
+    let milisegundos = msTotales % 1000;
+    msTotales = (msTotales - milisegundos) / 1000;
+    // segundos
+    let segundos = msTotales % 60;
+    msTotales = (msTotales - segundos) / 60;
+    // minutos
+    let minutos = msTotales % 60;
+    msTotales = (msTotales - minutos) / 60;
+    // horas
+    let horas = msTotales % 24;
+    let dias = (msTotales - horas) / 24;
+
+    return dias + " días, " + horas + " horas, " + minutos + " minutos, " + segundos + " segundos";
 
 }
