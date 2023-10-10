d320c4e840eb1985353900445df2fbab60a10356
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-10 10:31:49 +0200

    see the teacher exercises

--- ArraysEjemplos/231010_02.js
+++ ArraysEjemplos/231010_02.js
@@ -1,1 +1,11 @@
-let 
+let pruebas = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"];
+
+function desplazarElementos2(coleccion, inicio, longitud, posicion) {
+    let aux = coleccion.splice(inicio, longitud);
+    let final = coleccion.splice(posicion, coleccion.length - (longitud + 1));
+    console.warn(coleccion);
+    console.log(final);
+    return coleccion.concat;
+}
+
+
--- ArraysEjemplos/splice.js
+++ ArraysEjemplos/splice.js
@@ -4,3 +4,8 @@ function desplazarElementos(coleccion, inicio, longitud) {
     let aux = coleccion.splice(inicio, longitud);
     return coleccion.concat(aux);
 }
+
+for (let i = 0; i < personas.length; i++) {
+    console.log(personas[i]);
+}
+console.log(desplazarElementos(personas, 3, 4));
