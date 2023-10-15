cf2cb8679f16f478684f0784870d0a846f206671
Antonio <91885884+antonioc8021@users.noreply.github.com>
2023-10-15 20:48:49 +0200

    make again to search the error

--- Ejercicio1/ejer1.js
+++ Ejercicio1/ejer1.js
@@ -1,7 +1,11 @@
+// Tarea DAWC03.01 - Ejercicio 1
+// Antonio Costas Salazar
+
 window.onload = principal;
 
 function principal() {
-    document.getElementById("boton").onclick = manejadorClick();
+    document.getElementById("miBoton").onclick = manejadorClick;
+
 }
 
 function manejadorClick() {
