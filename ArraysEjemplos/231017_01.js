d81d67b63466e5a3c69f1a224ec23f1dcfdfc8ec
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 09:21:34 +0200

    works

--- ArraysEjemplos/231017_01.js
+++ ArraysEjemplos/231017_01.js
@@ -3,8 +3,7 @@
 let arrayNumeros = [24, 18, 3.4, 187, 1, 434, 2, 934, 500, 650, 7, 2, 140, 33];
 
 function ordenarAleatorio(a, b) {
-    return math.round(math.random() * (1 - (-1)) + (-1));
+    return Math.round(Math.random() * (1 + 2) + -1);
 }
 
-//console.log(arrayNumeros.sort(ordenarAleatorio));
-arrayNumeros.sort(ordenarAleatorio);
+console.log(arrayNumeros.sort(ordenarAleatorio));
