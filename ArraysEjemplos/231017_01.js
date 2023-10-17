b337f166ba46c7571340ce393d2910b72ce89473
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 09:47:41 +0200

    adding methods compare

--- ArraysEjemplos/231017_01.js
+++ ArraysEjemplos/231017_01.js
@@ -3,12 +3,18 @@
 let arrayNumeros = [24, 18, 3.4, 187, 1, 434, 2, 934, 500, 650, 7, 2, 140, 33];
 let arrayAlumnos = [["Ana", "Alonso"], ["Bea", "Pérez"], ["Antonio", "Sierra"], ["Jose", "Fuentes"]];
 
+function ordenarNumerosMenorAMayor(a, b) {
+    return a - b;
+}
+
 function ordenarAleatorio(a, b) {
     return Math.round(Math.random() * (1 + 2) + -1);
 }
 
-console.log(arrayNumeros.sort(ordenarAleatorio));
+console.log("Ordena de forma aleatoria: " + arrayNumeros.sort(ordenarAleatorio));
+console.log("Ordena de menor a mayor: " + arrayNumeros.sort(ordenarNumerosMenorAMayor));
 
 function ordenarPorNombre(a, b) {
+    // para comparar y ordenar por nombre tendré que trbajar con el primer elemento de a y b, es decir a[0] y b[0]
 
 }
