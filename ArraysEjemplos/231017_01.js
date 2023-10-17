0a40f15f79ca5f51c168a67a1b6b19e7ec6ba710
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 09:08:58 +0200

    don´t found need to fix that

--- ArraysEjemplos/231017_01.js
+++ ArraysEjemplos/231017_01.js
@@ -1,3 +1,10 @@
 // ejercicios propuestos por la diapositiva 24
 
-let numeros = []
+let arrayNumeros = [24, 18, 3.4, 187, 1, 434, 2, 934, 500, 650, 7, 2, 140, 33];
+
+function ordenarAleatorio(a, b) {
+    return math.round(math.random() * (1 - (-1)) + (-1));
+}
+
+//console.log(arrayNumeros.sort(ordenarAleatorio));
+arrayNumeros.sort(ordenarAleatorio);
