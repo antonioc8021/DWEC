0e516e7c67c8b1d9c390fa565a2677bb0ba2b670
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 09:59:12 +0200

    A little bit better

--- ArraysEjemplos/231017_01.js
+++ ArraysEjemplos/231017_01.js
@@ -11,10 +11,12 @@ function ordenarAleatorio(a, b) {
     return Math.round(Math.random() * (1 + 2) + -1);
 }
 
-console.log("Ordena de forma aleatoria: " + arrayNumeros.sort(ordenarAleatorio));
-console.log("Ordena de menor a mayor: " + arrayNumeros.sort(ordenarNumerosMenorAMayor));
-
 function ordenarPorNombre(a, b) {
     // para comparar y ordenar por nombre tendré que trbajar con el primer elemento de a y b, es decir a[0] y b[0]
-
+    return a[0].localCompare(b[0]);
 }
+
+
+console.log("Ordena de forma aleatoria: " + arrayNumeros.sort(ordenarAleatorio));
+console.log("Ordena de menor a mayor: " + arrayNumeros.sort(ordenarNumerosMenorAMayor));
+console.log("Ordenar los nombre de manera alfabética: " + arrayAlumnos.sort(ordenarPorNombre));
