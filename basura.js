43b8d1c25a2dd1fbc1ac9d95a8d43c99e78cae0d
Antonio <91885884+antonioc8021@users.noreply.github.com>
2023-10-18 21:26:54 +0200

    setTimeOut and setInterval examples

--- basura.js
+++ basura.js
@@ -4,3 +4,17 @@ let arrayNumeros = [1, 2, 3, 4, 5, 6, 7];
 // Math.random() * range + start;
 
 console.log(Math.random() * 4 + 3);
+
+
+setInterval(molestar, 500);
+setTimeout(decirHola, 2000);
+
+
+
+function molestar() {
+    console.warn("Soy un pesado");
+}
+
+function decirHola() {
+    console.log("Hola!");
+}
