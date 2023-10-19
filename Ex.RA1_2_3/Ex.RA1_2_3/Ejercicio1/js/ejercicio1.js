03c26ab50c65cfef2f5e5aba79f6ff0a1ef8864f
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-19 08:33:35 +0200

    examn

---
+++ Ex.RA1_2_3/Ex.RA1_2_3/Ejercicio1/js/ejercicio1.js
@@ -0,0 +1,15 @@
+// Nombre y apellidos
+
+window.onload = principal;
+
+function principal()
+{
+    document.getElementById("miBoton").onclick = manejadorClick;
+}
+
+function manejadorClick()
+{
+    document.getElementById("salida").innerHTML =
+                                        "Has escrito: " +
+                                        document.getElementById("entrada").value;
+}
---
+++ Ex.RA1_2_3/Ex.RA1_2_3/Ejercicio2/js/ejercicio2.js
@@ -0,0 +1,13 @@
+// Nombre y apellidos
+
+window.onload = principal;
+
+function principal()
+{
+    document.getElementById("Parar").onclick = manejadorClick;
+}
+
+function manejadorClick()
+{
+    console.log("manejadorClick");
+}
