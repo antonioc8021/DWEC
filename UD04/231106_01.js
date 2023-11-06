c61c90c364b63371265c3a5d22244c957d6a30ef
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 08:30:13 +0100

    created new files

--- UD04/231106_01.js
+++ UD04/231106_01.js
@@ -7,7 +7,3 @@ function principal() {
 function manejadorClick() {
     document.getElementById("salida").innerHTML = "Has escrito: " + document.getElementById("entrada").value;
 }
-
-function contarCaracter(palabra, caracter) {
-    return palabra.toLowerCase().length - palabra.toLowerCase().replaceAll(caracter.toLowerCase(),)
-}
