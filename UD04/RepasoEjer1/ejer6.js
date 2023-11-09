873deeab9fe4361c47e8b36f2e1526f597bb03ac
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-09 11:42:19 +0100

    make variables optionals

--- UD04/RepasoEjer1/ejer6.js
+++ UD04/RepasoEjer1/ejer6.js
@@ -7,3 +7,13 @@ function principal() {
 function manejadorClick() {
     document.getElementById("salida").innerHTML = "Has escrito: " + document.getElementById("entrada").value;
 }
+
+
+function crearPersona(nombres, apellidos, telefonos, edades) {
+    let nombre = nombres;
+    let apellido = apellidos;
+    let telefono = telefonos || '555';
+    let edad = edades || '0';
+    console.log(`Nombre: ${nombre}, apellido: ${apellido}, teléfono: ${telefono}, edad: ${edad}`);
+}
+
