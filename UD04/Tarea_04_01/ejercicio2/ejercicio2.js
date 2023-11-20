08eb2c6d676d6c8559176a84d30330e849098402
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-20 09:27:55 +0100

    creates ejer 2

---
+++ UD04/Tarea_04_01/ejercicio2/ejercicio2.js
@@ -0,0 +1,3 @@
+let electro1 = new Eectrodomestico();
+let electro2 = new Electrodomestico("1234", "teka", 23);
+
---
+++ UD04/Tarea_04_01/ejercicio2/electrodomestrico.js
@@ -0,0 +1,7 @@
+
+
+function Electrodomestico(id, modelo, consumo) {
+    this._id = id || "NOID";
+    this._modelo = modelo || "NOMOD";
+    this._consumo = consumo;
+}
