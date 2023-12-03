bfc0b244b633d880282734864a815e4350053161
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-12-03 15:14:52 +0100

    upgrade teachers work

--- UD04/Tarea_04_01/ejercicio2/ejercicio2.js
+++ UD04/Tarea_04_01/ejercicio2/ejercicio2.js
@@ -2,4 +2,6 @@ let electro1 = new Electrodomestico();
 let electro2 = new Electrodomestico("1234", "teka", 23);
 
 console.log(electro1);
+console.log(electro1.toString());
 console.log(electro2);
+console.log(electro2.toString());
--- UD04/Tarea_04_01/ejercicio2/electrodomestrico.js
+++ UD04/Tarea_04_01/ejercicio2/electrodomestrico.js
@@ -3,3 +3,48 @@ function Electrodomestico(id, modelo, consumo) {
     this._modelo = modelo || "NOMOD";
     this._consumo = consumo;
 }
+
+Electrodomestico.prototype = {
+    set id(nuevoId) {
+        nuevoId = nuevoId.toUpperCase();
+        if (this.verificarID(nuevoId)) {
+            this._id = nuevoId;
+        }
+    },
+    get id() {
+        return this._id;
+    },
+    set modelo(nuevoModelo) {
+        nuevoModelo = nuevoModelo.toUpperCase();
+        if (nuevoModelo.length >= 6) {
+            this._modelo = nuevoModelo;
+        }
+    },
+    get modelo() {
+        return this._modelo
+    },
+    set consumo(nuevoConsumo) {
+
+    }
+}
+
+Electrodomestico.prototype.toString = function () {
+    return "ELEC" + this._id + "; " + this._modelo + "; " + this._consumo + ";";
+}
+
+
+Electrodomestico.prototype.verificarID = function (id) {
+    let resultado = false;
+    if (id.startswith("ELEC - ") && id.length >= 10 && id.length <= 20) {
+        resultado = true;
+    }
+    return resultado;
+}
+
+Electrodomestico.prototype.calcularConsumo = function (horas) {
+    let resultado = -1;
+    if (horas >= 0) {
+        resultado = horas * this._consumo;
+    }
+    return resultado;
+}
