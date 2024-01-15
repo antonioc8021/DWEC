fe452b4d40283500a0995eb324a05d940ac976c2
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-15 12:08:46 +0100

    regular expresions

--- UD05/expresionesRegulares.js
+++ UD05/expresionesRegulares.js
@@ -1,6 +1,17 @@
 // Expresiones Regulares Antonio Costas, todo esto lo copio y lo pego en la terminal de un about:blank.
 
 // Verificar que un identificador que empiece por "ELEC - " y que continue con entre 3 y 8 dígitos.
-let expresionRegular = /^ELEC - [0-9]{3,8}$/; //funciona del tirón, let´s go
+let expresionRegular1 = /^ELEC - \d{3,8}$/; // el /d admite números.
+
+// Verificar que un identificador que empiece por "ELEC - " o "DISP - " y que continue con entre 3 y 8 dígitos.
+let expresionRegular2 = /^(ELEC - |DISP - )\d{3,8}$/;
+
+// Verificar un String que contenga 1 A mayúscula en cualquier parte de su contenido por lo menos.
+let expresionRegular3 = /A/; // al no poner ni inicio ni fin puede estar en cualquier sitio de la String.
+
+// Verificar un String que contenga 1 A mayúscula en cualquier parte de su contenido pero no más.
+let expresionRegular4 = /A{1,1}/;
+
+// Verificar un String que contenga 3 A mayúscula en cualquier parte de su contenido pero no más.
 
 
