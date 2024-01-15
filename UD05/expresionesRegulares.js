cf2b6364256a4edfa03947173e8bf0215f7ee2ba
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-15 08:57:40 +0100

    creates file expresionesRegulares

---
+++ UD05/expresionesRegulares.js
@@ -0,0 +1,6 @@
+// Expresiones Regulares Antonio Costas, todo esto lo copio y lo pego en la terminal de un about:blank.
+
+// Verificar que un identificador que empiece por "ELEC - " y que continue con entre 3 y 8 dígitos.
+let expresionRegular = /^ELEC - [0-9]{3,8}$/; //funciona del tirón, let´s go
+
+
