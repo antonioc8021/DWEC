ab76eb9c9b4f35ed937551cd545e693bbd56d0ee
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-23 09:46:13 +0100

    colores

---
+++ UD06/240123_01.js
@@ -0,0 +1,26 @@
+window.onload = principal;
+
+function principal() {
+    document.getElementById("miBoton").onclick = manejadorClick;
+}
+
+function manejadorClick() {
+    document.getElementById("salida").innerHTML = "Has escrito: " + document.getElementById("entrada").value;
+}
+
+function crearElemento(tagNombre, opciones) {
+    let elemento = document.createElement(tagNombre);
+    console.log(opciones);
+    if (opciones && opciones.atributos && typeof opciones.atributos === 'object') {
+        for (let clave in opciones.atributos) {
+            elemento.setAttribute(clave, opciones.atributos[clave]);
+        }
+    }
+
+    if (opciones && opciones.textoContenido) {
+        contenido = document.createTextNode(opciones.textoContenido);
+        elemento.appendChild(contenido);
+    }
+
+    return elemento;
+}
