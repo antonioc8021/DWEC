070976434549907082d64cca196ededc5d80225c
Antonio <91885884+antonioc8021@users.noreply.github.com>
2024-01-30 21:13:04 +0100

    estructure tarea

---
+++ UD06/Tarea/Propuesto_Tarea03/scripts/teclado.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", principal, false);
+
+// APARTADO 1
+function principal(e)
+{
+
+}
+
+// APARTADO 2
+function pulsarTecla(e)
+{
+    
+}
+
+function soltarTecla(e)
+{
+    
+}
+
+// APARTADO 3
+function entrarTecla(e)
+{
+
+}
+
+function salirTecla(e)
+{
+
+}
+
+// APARTADO 4
+function pulsarClick(e)
+{
+
+}
+
+function soltarClick(e)
+{
+
+}
---
+++ UD06/Tarea/Propuesto_Tarea04/scripts/Propuesto_Tarea04.js
@@ -0,0 +1,27 @@
+// Nombre y apellidos
+
+
+//// MANEJADORES DE EVENTOS
+function entroDiv()
+{
+
+}
+
+function salgoDiv()
+{
+
+}
+
+function clickDiv()
+{
+    
+}
+
+//// HERRAMIENTAS
+
+// No modifiques la siguiente función
+function colorAleatorio()
+{
+    let colores =["red","yellow","blue","green","orange","blue","black","pink","purple","gold","olive","navy","beige","gray","aqua","cyan"];
+    return colores[Math.round(Math.random()*colores.length)];
+}
