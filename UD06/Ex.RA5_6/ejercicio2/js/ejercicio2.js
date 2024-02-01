0b13685e4f59e015666edb80bcd93f1646aca045
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-02-01 08:35:02 +0100

    examen

---
+++ UD06/Ex.RA5_6/ejercicio2/js/ejercicio2.js
@@ -0,0 +1,17 @@
+// Nombre y apellidos
+
+function manejadorClick(e) {
+
+}
+
+function entroDiv(e) {
+	
+}
+
+function salgoDiv(e) {
+	
+}
+
+function escribir(e) {
+
+}
---
+++ UD06/Ex.RA5_6/ejercicio3/js/ejercicio3.js
@@ -0,0 +1,2 @@
+// Nombre y apellidos
+
